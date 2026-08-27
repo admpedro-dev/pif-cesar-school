18040312b8f083c6a23722785b560b2c8dde86a8
admpedro-dev <admpedro.dev@gmail.com>
2026-08-26 22:56:08 -0300

    Create questao1.c

---
+++ ListaDeExercicios-Cap1/RespostasEmCódigo/questao1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int anoNascimento = 2005;
+    
+    printf("Meu ano de nascimento é: %d\n", anoNascimento);
+    
+    system("PAUSE");
+    return 0;
+}
