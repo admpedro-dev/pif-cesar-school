bad02d41c4e7625c0732afcfdc0361aaf3b172f4
admpedro-dev <admpedro.dev@gmail.com>
2026-08-26 22:58:48 -0300

    Create questao3.c

---
+++ ListaDeExercicios-Cap1/RespostasEmCódigo/questao3.c
@@ -0,0 +1,19 @@
+/* Esse programa mostra o uso de comentários em várias linhas
+ * e mostra também o uso de comentários em uma única linha
+ *
+ * Demonstração de comentários - Questão 03
+ *************************************************************/
+ 
+/* Questao03.c */
+ 
+#include <stdio.h>     /* Para printf() */
+#include <stdlib.h>    /* Para system() */
+ 
+int main()             /* Função main */
+{                      /* início do corpo da função main */
+    printf("Exemplo de programa com comentários corretamente documentado.\n"); /* Chamada à função printf */
+    
+    system("PAUSE");   /* Chamada à função system */
+    
+    return 0;          /* Retorna sucesso */
+}                      /* Fim do corpo da função main */
