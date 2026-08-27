3d590c86d19277e011f285d27643618b46fd0cdf
admpedro-dev <admpedro.dev@gmail.com>
2026-08-26 22:57:01 -0300

    Create questao2.c

---
+++ ListaDeExercicios-Cap1/RespostasEmCódigo/questao2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    float euler = 2.71828f;
+    
+    printf("O número de Euler com 3 casas decimais: %.3f\n", euler);
+    
+    system("PAUSE");
+    return 0;
+}
+ 
